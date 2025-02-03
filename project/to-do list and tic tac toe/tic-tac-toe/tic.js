0aa429f7c54dddaabb1105af35a207c3919e69c1
Mahak Gupta <mahakgupta10a24@gmail.com>
2025-02-03 15:10:23 +0530

    added functinality of button

--- project/to-do list and tic tac toe/tic-tac-toe/tic.js
+++ project/to-do list and tic tac toe/tic-tac-toe/tic.js
@@ -1,12 +1,12 @@
 let player1=document.querySelector(".player1");
 let player2=document.querySelector(".player2");
+let h1=document.querySelector("h1");
 player1.addEventListener("click",()=>{
-    console.log("player1 is playing");
-    let div=document.createElement("div");
-    div.innerText="now player1 will play"
-    player1.appendChild(div)
+    h1.innerText="now player1 will play"
+   h1.style.backgroundColor="greenyellow";
 })
 
 player2.addEventListener("click",()=>{
-    console.log("player2 is playing");
+    h1.innerText="now player2 will play"
+   h1.style.backgroundColor="green";
 })
