9b7862739239a278ff5c5464a293b5c327e9fc60
Mahak Gupta <mahakgupta10a24@gmail.com>
2025-02-04 17:30:24 +0530

    added redirection

--- project/project-random color generation/randon.js
+++ project/project-random color generation/randon.js
@@ -1,6 +1,7 @@
 let btn=document.querySelector("button");
 let div=document.querySelector(".color");
 let h1=document.querySelector("h1");
+let button=document.querySelector(".redirect")
 btn.addEventListener("click",()=>{
     h1.innerText=(`rgb(${randomColor()},${randomColor()},${randomColor()})`)
     div.style.backgroundColor=(`rgb(${randomColor()},${randomColor()},${randomColor()})`)
@@ -9,4 +10,14 @@ btn.addEventListener("click",()=>{
 let randomColor=function(){
     let color1= Math.floor(Math.random()*255);
     return color1;
+}
+
+button.addEventListener("click", ()=>{
+    // let a=document.createElement("a");
+    // a.href="https://stackoverflow.com/questions/57394024/redirecting-page-from-form-with-addeventlistener-method"
+    redirect();
+})
+
+function redirect(){
+    window.location.href ="https://stackoverflow.com/questions/57394024/redirecting-page-from-form-with-addeventlistener-method"
 }
