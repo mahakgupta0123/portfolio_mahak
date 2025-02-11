be2776dd67bb1a16ed052aaf1178d692310167d5
Mahak Gupta <mahakgupta10a24@gmail.com>
2025-02-11 21:17:31 +0530

    code updated

---
+++ practice/practice.js
@@ -0,0 +1,11 @@
+let btn=document.querySelector(".btn1");
+let btn1=document.querySelector(".btn2");
+
+btn.addEventListener("click",()=>{
+    alert("btn1 is clicked");
+
+})
+
+btn1.addEventListener("click",()=>{
+    alert("btn2 is clicked");
+})
